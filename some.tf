e1fda736adb0a3f315662179619cad636dcad51c
sandeepsomana11 <sandeepsomana11@gmail.com>
2024-08-30 22:01:27 +0530

    Delete some.tf

--- some.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "sub" {
-    name = "sub01"
-    address_prefixes = ["10.1.1.0/24"]
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    resource_group_name = azurerm_resource_group.rg.name
-  
-}
